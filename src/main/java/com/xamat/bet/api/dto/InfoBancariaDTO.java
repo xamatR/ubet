078a4e2f3180f7fe4288f3ac5762bec44704a8f9
gabriel <gabriel@iberwandobrasil.onmicrosoft.com>
2024-07-02 19:53:15 -0300

    dto info bancaria

--- src/main/java/com/xamat/bet/api/dto/InfoBancariaDTO.java
+++ src/main/java/com/xamat/bet/api/dto/InfoBancariaDTO.java
@@ -1,6 +1,16 @@
 package com.xamat.bet.api.dto;
 
 public class InfoBancariaDTO {
-    
-    
+
+    private Long id;
+
+    private String agencia;
+    private String conta;
+    private String banco;
+    private String tipoConta;
+
+    public static InfoBancariaDTO create(InfoBancaria infoBancaria) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(infoBancaria, InfoBancariaDTO.class);
+    }
 }
