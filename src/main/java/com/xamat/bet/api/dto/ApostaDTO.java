ae869c021a9cc09730780ee45e2833e2a2f73680
gabriel <gabriel@iberwandobrasil.onmicrosoft.com>
2024-07-02 19:51:03 -0300

    dto aposta

--- src/main/java/com/xamat/bet/api/dto/ApostaDTO.java
+++ src/main/java/com/xamat/bet/api/dto/ApostaDTO.java
@@ -1,5 +1,21 @@
 package com.xamat.bet.api.dto;
 
+import org.modelmapper.ModelMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.xamat.bet.api.model.Aposta;
+
+@Data
+@NoArgsConstructor
 public class ApostaDTO {
-    
+    private Long id;
+    private LocalDateTime data;
+    private float valor;
+    private String status;
+
+    public static ApostaDTO create(Aposta aposta) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(aposta, ApostaDTO.class);
+    }
 }
